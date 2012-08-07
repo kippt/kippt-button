0de235f67232aa988cd7b81605d91bea1a28584a
sprt <hellosprt@gmail.com>
2012-08-07 21:15:43 +0200

    Remove the jQuery dependency, fixing #1

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Button opens Kippt's extension page with specific parameters.
 
 ## Install
 
-You can install "Save to Kippt" button to any site without the code on this site. Include the following file:
+You can install the "Save to Kippt" button to any site without the code on this site. Include the following file:
 
     <script>(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="http://addons.kippt.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","kippt-wjs"));</script>
 
@@ -21,11 +21,6 @@ Attributes:
 - _data-source_ - (Optional) Source, e.g. page domain
 - _data-via_ - (Optional) Source clip URI if you want to reference another clip inside Kippt (e.g. /api/clips/1337/)
 
-## Requirements
-
-- jQuery / Zepto
-
 ## TODO
 
-- Remove $ dependency
 - Add HTTPS enabled CDN
--- widgets.js
+++ widgets.js
@@ -1,39 +1,59 @@
-$(function() {
-    // CSS
-    var css = ".kippt-save-button {\
-                    background:url(http://addons.kippt.com/save-button/img/kippt-btn.png) no-repeat;\
-                    height:20px;\
-                    width:62px;\
-                    display:block;\
-                    position:relative;\
-                    margin-top:5px;\
-                }\
-                .kippt-save-button:hover {\
-                    background-position:0 -30px;\
-                }\
-                .kippt-save-button:active {\
-                    background-position:0 -60px;\
-                }\
-                .kippt-save-button span {\
-                    display: none;\
-                }";
-    $("head").append("<style>"+css+"</style>");
+(function() {
+    "use strict";
     
-    // Loading
-    $(".kippt-save-button").each(function(i, elem){
-        $(elem).html("<span>Save to Kippt</span>")
+    var css = "\
+    .kippt-save-button {\
+        background: url(http://addons.kippt.com/save-button/img/kippt-btn.png)\
+                    no-repeat;\
+        height: 20px;\
+        width: 62px;\
+        display: block;\
+        position: relative;\
+        margin-top: 5px;\
+    }\
+    .kippt-save-button:hover {\
+        background-position: 0 -30px;\
+    }\
+    .kippt-save-button:active {\
+        background-position: 0 -60px;\
+    }";
+    
+    var style = document.createElement("style");
+    style.innerHTML = css;
+    document.head.appendChild(style);
+    
+    var saveButtons = document.querySelectorAll(".kippt-save-button");
+    saveButtons = [].slice.call(saveButtons);
+    
+    saveButtons.forEach(function (el) {
+        el.innerHTML = "";
     });
     
-    // Handle click
-    $(".kippt-save-button").on("click", function(e){
-        e.preventDefault();
-        var elem = e.currentTarget;
-        var url = encodeURIComponent($(elem).data("url")),
-            title= encodeURIComponent($(elem).data("title")),
-            source = encodeURIComponent($(elem).data("source")),
-            via = encodeURIComponent($(elem).data("via"));
-        
-        var windowUrl = "https://kippt.com/extensions/new?url="+ url +"&title="+ title +"&source="+ source +"&via="+ via;
-        window.open(windowUrl, "kippt-popup", "location=no,menubar=no,status=no,titlebar=no,scrollbars=no,width=420,height=192");
+    var popupFeatures = "location=no,menubar=no,status=no,titlebar=no," +
+        "scrollbars=no,width=420,height=192";
+    
+    function onClick(e) {
+        if (saveButtons.indexOf(e.target) !== -1) {
+            e.preventDefault();
+            
+            var data = e.target.dataset;
+            
+            for (var key in e.target.dataset) {
+                console.log(key);
+                data[key] = encodeURIComponent(data[key]);
+            }
+            
+            var windowURL = "https://kippt.com/extensions/new" +
+                "?url=" + data.url +
+                "&title=" + data.title +
+                "&source=" + data.source +
+                "&via=" + data.via;
+            
+            window.open(windowURL, "kippt-popup", popupFeatures);
+        }
+    }
+    
+    saveButtons.forEach(function (el) {
+        el.addEventListener("click", onClick, false);
     });
-});
+})();
