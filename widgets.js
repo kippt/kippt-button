191b99ae24762b0a04f0522382051326bf0871d9
Yu-Cheng Chuang <ducksteven@gmail.com>
2012-08-08 01:08:58 +0800

    access data attributes with native DOM DataSet API

--- widgets.js
+++ widgets.js
@@ -32,10 +32,10 @@ $(function() {
     $(".kippt-save-button").on("click", function(e){
         e.preventDefault();
         var elem = e.currentTarget;
-        var url = encodeURIComponent($(elem).data("url")),
-            title= encodeURIComponent($(elem).data("title")),
-            source = encodeURIComponent($(elem).data("source")),
-            via = encodeURIComponent($(elem).data("via"));
+        var url = encodeURIComponent(elem.dataset.url),
+            title = encodeURIComponent(elem.dataset.title),
+            source = encodeURIComponent(elem.dataset.source),
+            via = encodeURIComponent(elem.dataset.via);
         
         var windowUrl = "https://kippt.com/extensions/new?url="+ url +"&title="+ title +"&source="+ source +"&via="+ via;
         window.open(windowUrl, "kippt-popup", "location=no,menubar=no,status=no,titlebar=no,scrollbars=no,width=420,height=192");
