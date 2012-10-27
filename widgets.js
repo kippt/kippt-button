920f4bd2582c66ffa2f1b4ddbd2ad5b5889df919
Alex Robinson <alextrob@gmail.com>
2012-10-27 14:13:59 +1000

    Removed jQuery dependency.
    Tested in:
    • Chrome v22 (Mac, Windows)
    • Safari 6.0.1 (Mac)
    • Safari on iOS 6.0
    • Firefox 16 (Mac, Windows)
    • IE8

--- widgets.js
+++ widgets.js
@@ -1,5 +1,15 @@
-$(function() {
-    // CSS
+function kipptReady(f){/in/.test(document.readyState)?setTimeout('kipptReady('+f+')',9):f()} // Ready function based on http://dustindiaz.com/smallest-domready-ever
+kipptReady(function() {
+    function saveToKippt(e) {
+        var url = e.getAttribute("data-url");
+        var title = e.getAttribute("data-title");
+        var source = e.getAttribute("data-source");
+        var via = e.getAttribute("data-via");    
+        var windowUrl = "https://kippt.com/extensions/new?url="+ url +"&title="+ title +"&source="+ source +"&via="+ via;
+        window.open(windowUrl, "_blank", "location=no,menubar=no,status=no,titlebar=no,scrollbars=no,width=420,height=192");
+        return false;
+    }
+    
     var css = ".kippt-save-button {\
                     background:url(https://s3.amazonaws.com/addons.kippt.com/save-button/img/kippt-btn.png) no-repeat;\
                     height:20px;\
@@ -17,23 +27,19 @@ $(function() {
                 .kippt-save-button span {\
                     display: none;\
                 }";
-    $("head").append("<style>"+css+"</style>");
-    
-    // Loading
-    $(".kippt-save-button").each(function(i, elem){
-        $(elem).html("<span>Save to Kippt</span>")
-    });
-    
-    // Handle click
-    $(".kippt-save-button").on("click", function(e){
-        e.preventDefault();
-        var elem = e.currentTarget;
-        var url = encodeURIComponent($(elem).data("url")),
-            title= encodeURIComponent($(elem).data("title")),
-            source = encodeURIComponent($(elem).data("source")),
-            via = encodeURIComponent($(elem).data("via"));
         
-        var windowUrl = "https://kippt.com/extensions/new?url="+ url +"&title="+ title +"&source="+ source +"&via="+ via;
-        window.open(windowUrl, "kippt-popup", "location=no,menubar=no,status=no,titlebar=no,scrollbars=no,width=420,height=192");
-    });
+    var cssDiv = document.createElement('div');
+    cssDiv.innerHTML = "<p>&nbsp;</p><style type='text/css'>" + css + "</style>";
+    document.getElementsByTagName("head")[0].appendChild(cssDiv.childNodes[1]);
+        
+    var links = document.getElementsByTagName("a");
+    for (var i = links.length - 1; i >= 0; i--) {
+        if(links[i].className === "kippt-save-button") {
+            links[i].innerHTML = "<span>Save to Kippt</span>";
+            links[i].onclick = function() {
+                saveToKippt(this);
+                return false;
+            };
+        }
+    };    
 });
